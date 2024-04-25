ba038c5da4c7bb956730f1a943b243eff9237cc4
GabrielMena8 <114101881+GabrielMena8@users.noreply.github.com>
2024-04-24 21:26:09 -0400

    inicio

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+
+import styles from './App.module.css'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1 className={
+        styles.title
+      }>Clima</h1>
+
+      <div className={
+        styles.container
+      }>
+        <p>1aaaaaaaaaaaaa</p>
+
+      </div>
+
+    </>
+  )
+}
+
+export default App
